3b806853da80f82abef68baa34daa6dd14f8c3f6
hua.xu <1013299500@qq.com>
2018-08-10 10:32:13 +0800

    fix: change some vulnerabilities in package

--- src/chaos.ts
+++ src/chaos.ts
@@ -46,8 +46,8 @@ const cjkANS = /([\u2e80-\u2eff\u2f00-\u2fdf\u3040-\u309f\u30a0-\u30ff\u3100-\u3
 const ansCJK = /([A-Za-z0-9`~\$%\^&\*\-=\+\\\|/!;:,\.\?\u00a1-\u00ff\u2022\u2026\u2027\u2150-\u218f])([\u2e80-\u2eff\u2f00-\u2fdf\u3040-\u309f\u30a0-\u30ff\u3100-\u312f\u3200-\u32ff\u3400-\u4dbf\u4e00-\u9fff\uf900-\ufaff])/g;
 
 //add ansSymbol to match end of the sentence.
-const ansSymbol = /([A-Za-z0-9`])([~!;,，。\)\.\?\u2026])/g;
-const Symbolans = /([~!;,，。\(\.\?\u2026])([A-Za-z0-9])/g;
+const ansSymbol = /([A-Za-z0-9`])([~!;,，。\)\?\u2026])/g;
+const Symbolans = /([~!;,，。\(\?\u2026])([A-Za-z0-9])/g;
 
 const enCJK = /([\u2e80-\u2eff\u2f00-\u2fdf\u3040-\u309f\u30a0-\u30ff\u3100-\u312f\u3200-\u32ff\u3400-\u4dbf\u4e00-\u9fff\uf900-\ufaff])([A-Za-z0-9])/g;
 const CJKen = /([A-Za-z0-9)])([\u2e80-\u2eff\u2f00-\u2fdf\u3040-\u309f\u30a0-\u30ff\u3100-\u312f\u3200-\u32ff\u3400-\u4dbf\u4e00-\u9fff\uf900-\ufaff])/g;
